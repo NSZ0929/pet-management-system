18f26608d12164e9c76024ffe21991abdc6ea56b
nieshengzhi <shengzhinie@163.com>
2026-03-19 22:42:08 +0800

    add

--- backend/pet-health/src/main/java/com/pethealth/pet_health/PetHealthApplication.java
+++ backend/pet-health/src/main/java/com/pethealth/pet_health/PetHealthApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableScheduling 
 public class PetHealthApplication {
 
 	public static void main(String[] args) {
